2a2a71cda43d23e4c89a870f2b9310ff696f90c9
glauco.r3 <glauco.r3@gmail.com>
2026-03-13 22:44:20 -0300

    Leitura

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -43,6 +43,72 @@ sap.ui.define([
                     oItems.filter(aTableSearchState);
                 }
 
+            },
+            /* ------------------------------------------------------- */
+            /* Ler Matnr da linha marcada, buscar no backend e filtrar */
+            /* ------------------------------------------------------- */
+            onClienteReadSelected: function(){
+                let oModel = this.getView().getModel();
+                let oTable = this.byId("table");
+                
+                //aItems[i] = /MaterialSet(Bukrs='1000',Matnr='MAT0002')
+                // let aItems = oTable.getSelectedContextPaths();
+
+                let aSelectedItems = oTable.getSelectedItems();
+
+                if (!!aSelectedItems && aSelectedItems.length > 1) {
+                    sap.m.MessageToast.show('Selecionar apenas uma linha');
+                    return;
+                } else {
+
+                  let aFilters = [];
+
+                  aSelectedItems.forEach(function(oItem){
+                      let oData = oItem.getBindingContext().getObject();
+                      aFilters.push(
+                          new Filter("Bukrs", FilterOperator.EQ, oData.Bukrs)
+                      );
+                      aFilters.push(
+                          new Filter("Matnr", FilterOperator.EQ, oData.Matnr)
+                      );
+                  });
+
+                  oModel.read("/MaterialSet", {
+                      filters: aFilters,
+                      success: function(oData){
+                          let aFilters = [];
+                          aFilters.push(
+                            new Filter("Bukrs", FilterOperator.EQ, oData.results[0].Bukrs)
+                          );
+                          aFilters.push(
+                            new Filter("Matnr", FilterOperator.EQ, oData.results[0].Matnr)
+                          );
+                          let oItems = oTable.getBinding('items');
+                          oItems.filter(aFilters);
+                          console.log(oData);
+                      },
+                      error: function(oError){
+                          console.log(oError);
+                      }
+                  });
+                }
+            },
+            onClienteReadAllBindAggregation: function(){
+                let oModel = this.getView().getModel();
+                let oTable = this.byId('table');
+                let bindingInfo = oTable.getBindingInfo('items');
+                let aFilters = [];
+
+                oTable.bindAggregation('items',{
+                    model: bindingInfo.model,
+                    path: '/MaterialSet',
+                    template: bindingInfo.template,
+                    sorter: [
+                        new sap.ui.model.Sorter("Menge", true)
+                    ],
+                    filter: aFilters
+                });
+
             }
         });
     });
