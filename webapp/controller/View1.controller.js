2fa6a481e5a21185b38d1ee3ee5c53dd56e47496
glauco.r3 <glauco.r3@gmail.com>
2026-03-13 21:07:16 -0300

    filter, sort

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,14 +1,47 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("cadmatv1.controller.View1", {
             onInit: function () {
+
+            },
+            /* -------------------------------------------------- */
+            /* Métodos customizdos                                */
+            /* -------------------------------------------------- */
+            onSortItems: function (oEvent) {
+                // let campo = 'Matnr';
+                // let oSorter = new sap.ui.model.Sorter({
+                //     path: campo,
+                //     descending: true
+                // });
+                let oSorter = new sap.ui.model.Sorter("Matnr", true);
+
+                let oTable = this.byId('table');
+                let oItems = oTable.getBinding('items');
+                oItems.sort(oSorter);
+                
+            },
+            onSearchCustom: function(oEvent){
+                let aTableSearchState = [];
+                let sQuery = oEvent.getParameter('query');
+
+                if (sQuery && sQuery.length > 0) {
+                    aTableSearchState = [
+                        new Filter('Matnr', FilterOperator.Contains, sQuery)
+                    ];
+
+                    let oTable = this.byId('table');
+                    let oItems = oTable.getBinding('items');
+                    oItems.filter(aTableSearchState);
+                }
 
             }
         });
